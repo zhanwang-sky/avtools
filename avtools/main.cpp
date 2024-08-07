e527174af6effd8a25553ca291b9c9a97812519b
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2024-08-07 14:38:53 +0800

    Initial Commit

---
+++ avtools/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  avtools
+//
+//  Created by zhanwang-sky on 2024/8/7.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
